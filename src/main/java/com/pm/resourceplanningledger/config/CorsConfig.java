ddc38781e9da9ccdfe9e8b5a70861cc486f7505a
Pranav Thippannappa Pattanashetty <ppattana@iu.edu>
2026-04-30 11:12:26 -0400

    minor github  fixes

--- src/main/java/com/pm/resourceplanningledger/config/CorsConfig.java
+++ src/main/java/com/pm/resourceplanningledger/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.pm.resourceplanningledger.client;
+package com.pm.resourceplanningledger.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
